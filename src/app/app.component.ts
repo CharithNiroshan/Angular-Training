451a55d04d8228c1e3d5d340dcad154bf406df84
CharithNiroshan <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>
2022-11-23 01:04:00 +0530

    Created Post Service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {PostService} from "./services/post/post.service"
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'GamesApp';
+
+  constructor(private post: PostService) {
+    this.post.getPosts().subscribe(data => console.log(data))
+  }
 }
